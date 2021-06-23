eb0549775643385c7dc0e61f48ad29584807828a
joser <joser@pineal.ai>
2021-06-23 11:56:12 +0800

    update swift package tool version 5.1 and update depend new version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 
@@ -16,7 +16,7 @@ let package = Package(
             targets: ["linenoiseDemo"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Quick/Nimble.git", from: "8.0.0")
+        .package(url: "https://github.com/Quick/Nimble.git", from: "9.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
